bf8a373256d7ff27acabe05413089fef944ec2e3
memenopa <lobothemainman4@gmail.com>
2025-05-09 04:36:00 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const images = document.querySelectorAll('.grid img');
-    images.forEach((img, index) => {
-        img.style.animation = `fadeIn 1s ease-out ${index * 0.1}s forwards`;
-        img.style.opacity = 0;
-    });
-});
